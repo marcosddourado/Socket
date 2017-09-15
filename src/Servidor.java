2b8e9349d1380643e0851fdec6da15a90c63b65a
Marcos Dourado <marcosdourado92@gmail.com>
2017-09-15 14:07:56 -0300

    refactoring server

--- src/Servidor.java
+++ src/Servidor.java
@@ -4,6 +4,8 @@ import java.net.*;
 import java.util.Scanner;
 
 public class Servidor {
+	private static final int NUMERO2 = 2;
+	private static final int NUMERO1 = 1;
 	private static final int OPERACAO = 0;
 
 	public static void main(String args[]) throws IOException {
@@ -20,10 +22,13 @@ public class Servidor {
 		while (inputCliente.hasNextLine()) {
 			String[] parts = inputCliente.nextLine().split(" ");
 			System.out.println("op " + parts[OPERACAO]);
+			int num1 = Integer.parseInt(parts[NUMERO1]);
+			int num2 = Integer.parseInt(parts[NUMERO2]);
 
 			if (parts[OPERACAO].equals("add")) {
-				saida.println("0 " + Integer.parseInt(parts[1]) + Integer.parseInt(parts[2]));
-				System.out.println("0 " + Integer.parseInt(parts[1]) + Integer.parseInt(parts[2]));
+				int soma = num1 + num2;
+				saida.println("0 " +  soma);
+				System.out.println("0 " + soma);
 			}
 
 		}
