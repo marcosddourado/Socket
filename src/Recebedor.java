8612b3b2ca8e74254500c463821a72318778d60c
Marcos Dourado <marcosdourado92@gmail.com>
2017-09-18 15:48:47 -0300

    sending answers to all the ips

--- src/Recebedor.java
+++ src/Recebedor.java
@@ -2,18 +2,31 @@ import java.io.InputStream;
 import java.util.Scanner;
 
 public class Recebedor implements Runnable {
- 
-   private InputStream servidor;
- 
-   public Recebedor(InputStream servidor) {
-     this.servidor = servidor;
-   }
- 
-   public void run() {
-     // recebe msgs do servidor e imprime na tela
-     Scanner s = new Scanner(this.servidor);
-     while (s.hasNextLine()) {
-       System.out.println(s.nextLine());
-     }
-   }
- }
+
+	private InputStream servidor;
+	private static final int RESPOSTA = 1;
+	private static final int STAUS_RESPOSTA = 0;
+
+	public Recebedor(InputStream servidor) {
+		this.servidor = servidor;
+	}
+
+	public void run() {
+		// recebe msgs do servidor e imprime na tela
+		Scanner inputServidor = new Scanner(this.servidor);
+		while (inputServidor.hasNextLine()) {
+//			System.out.println(inputServidor.nextLine());
+
+			String[] resposta = inputServidor.nextLine().split(" ");
+			imprimirReposta(resposta);
+		}
+	}
+
+	private static void imprimirReposta(String[] resposta) {
+		if (resposta[STAUS_RESPOSTA].equals("0")) {
+			System.out.println("Resposta: " + resposta[RESPOSTA]);
+		} else {
+			System.out.println("ERRO!");
+		}
+	}
+}
--- src/TrataCliente.java
+++ src/TrataCliente.java
@@ -1,22 +1,52 @@
 import java.io.InputStream;
+import java.io.PrintStream;
 import java.util.Scanner;
 
 public class TrataCliente implements Runnable {
- 
-   private InputStream cliente;
-   private Servidor servidor;
- 
-   public TrataCliente(InputStream cliente, Servidor servidor) {
-     this.cliente = cliente;
-     this.servidor = servidor;
-   }
- 
-   public void run() {
-     // quando chegar uma msg, distribui pra todos
-     Scanner s = new Scanner(this.cliente);
-     while (s.hasNextLine()) {
-       servidor.distribuiMensagem(s.nextLine());
-     }
-     s.close();
-   }
- }
+
+	private InputStream cliente;
+	private Servidor servidor;
+	private static final int NUMERO2 = 2;
+	private static final int NUMERO1 = 1;
+	private static final int OPERACAO = 0;
+
+	public TrataCliente(InputStream cliente, Servidor servidor) {
+		this.cliente = cliente;
+		this.servidor = servidor;
+	}
+
+	public void run() {
+		// quando chegar uma msg, distribui pra todos
+		Scanner inputCliente = new Scanner(this.cliente);
+		while (inputCliente.hasNextLine()) {
+			// servidor.distribuiMensagem(inputCliente.nextLine());
+
+			String[] parts = inputCliente.nextLine().split(" ");
+			System.out.println("op " + parts[OPERACAO]);
+			int num1 = Integer.parseInt(parts[NUMERO1]);
+			int num2 = Integer.parseInt(parts[NUMERO2]);
+
+			processarRequisicao(parts, num1, num2);
+
+		}
+		inputCliente.close();
+	}
+
+	private void processarRequisicao(String[] parts, int num1, int num2) {
+		if (parts[OPERACAO].equals("add")) {
+			int soma = num1 + num2;
+			servidor.distribuiMensagem("0 " + soma);
+		} else if (parts[OPERACAO].equals("sub")) {
+			int sub = num1 - num2;
+			servidor.distribuiMensagem("0 " + sub);
+		} else if (parts[OPERACAO].equals("mul")) {
+			int mul = num1 * num2;
+			servidor.distribuiMensagem("0 " + mul);
+		} else if (parts[OPERACAO].equals("div") && num2 != 0) {
+			int div = num1 / num2;
+			servidor.distribuiMensagem("0 " + div);
+		} else {
+			servidor.distribuiMensagem("1");
+		}
+	}
+}
