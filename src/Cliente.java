3bf7a5d70020c24073d5d5fb4cb60394acd96303
Marcos Dourado <marcosdourado92@gmail.com>
2017-09-18 15:07:24 -0300

    implementing multi conversation

--- src/Cliente.java
+++ src/Cliente.java
@@ -4,40 +4,76 @@ import java.util.Scanner;
 import java.io.PrintStream;
 
 public class Cliente {
-	private static final int RESPOSTA = 1;
-	private static final int STAUS_RESPOSTA = 0;
-
 	public static void main(String[] args) throws UnknownHostException, IOException {
-		Socket cliente = new Socket("127.0.0.1", 12345);
-		System.out.println("O cliente se conectou ao servidor!");
-		System.out.println("Faca uma requisicao ou escreva SAIR");
+		// dispara cliente
+		new Cliente("127.0.0.1", 12345).executa();
+	}
 
+	private String host;
+	private int porta;
+
+	public Cliente(String host, int porta) {
+		this.host = host;
+		this.porta = porta;
+	}
+
+	public void executa() throws UnknownHostException, IOException {
+		Socket cliente = new Socket(this.host, this.porta);
+		System.out.println("O cliente se conectou ao servidor!");
+
+		// thread para receber mensagens do servidor
+		Recebedor r = new Recebedor(cliente.getInputStream());
+		new Thread(r).start();
+
+		// lê msgs do teclado e manda pro servidor
 		Scanner teclado = new Scanner(System.in);
 		PrintStream saida = new PrintStream(cliente.getOutputStream());
-		Scanner inputServidor = new Scanner(cliente.getInputStream());
-
-		while (true) {
-			String entrada = teclado.nextLine();
-			if(!entrada.equalsIgnoreCase("SAIR")) {
-				saida.println(entrada);
-			} else {
-				break;
-			}
-			
-			String[] resposta = inputServidor.nextLine().split(" ");
-			imprimirReposta(resposta);
+		while (teclado.hasNextLine()) {
+			saida.println(teclado.nextLine());
 		}
 
 		saida.close();
 		teclado.close();
 		cliente.close();
 	}
-
-	private static void imprimirReposta(String[] resposta) {
-		if(resposta[STAUS_RESPOSTA].equals("0")) {
-			System.out.println("Resposta: " + resposta[RESPOSTA]);
-		} else {
-			System.out.println("ERRO!");	
-		}
-	}
 }
+
+// public class Cliente {
+// private static final int RESPOSTA = 1;
+// private static final int STAUS_RESPOSTA = 0;
+//
+// public static void main(String[] args) throws UnknownHostException,
+// IOException {
+// Socket cliente = new Socket("127.0.0.1", 12345);
+// System.out.println("O cliente se conectou ao servidor!");
+// System.out.println("Faca uma requisicao ou escreva SAIR");
+//
+// Scanner teclado = new Scanner(System.in);
+// PrintStream saida = new PrintStream(cliente.getOutputStream());
+// Scanner inputServidor = new Scanner(cliente.getInputStream());
+//
+// while (true) {
+// String entrada = teclado.nextLine();
+// if(!entrada.equalsIgnoreCase("SAIR")) {
+// saida.println(entrada);
+// } else {
+// break;
+// }
+//
+// String[] resposta = inputServidor.nextLine().split(" ");
+// imprimirReposta(resposta);
+// }
+//
+// saida.close();
+// teclado.close();
+// cliente.close();
+// }
+//
+// private static void imprimirReposta(String[] resposta) {
+// if(resposta[STAUS_RESPOSTA].equals("0")) {
+// System.out.println("Resposta: " + resposta[RESPOSTA]);
+// } else {
+// System.out.println("ERRO!");
+// }
+// }
+// }
--- src/Servidor.java
+++ src/Servidor.java
@@ -1,53 +1,100 @@
 import java.io.IOException;
 import java.io.PrintStream;
 import java.net.*;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Servidor {
-	private static final int NUMERO2 = 2;
-	private static final int NUMERO1 = 1;
-	private static final int OPERACAO = 0;
+	 
+	   public static void main(String[] args) throws IOException {
+	     // inicia o servidor
+	     new Servidor(12345).executa();
+	   }
+	   
+	   private int porta;
+	   private ArrayList<PrintStream> clientes;
+	   
+	   public Servidor (int porta) {
+	     this.porta = porta;
+	     this.clientes = new ArrayList<PrintStream>();
+	   }
+	   
+	   public void executa () throws IOException {
+	     ServerSocket servidor = new ServerSocket(this.porta);
+	     System.out.println("Porta 12345 aberta!");
+	     
+	     while (true) {
+	       // aceita um cliente
+	       Socket cliente = servidor.accept();
+	       System.out.println("Nova conexão com o cliente " +   
+	         cliente.getInetAddress().getHostAddress()
+	       );
+	       
+	       // adiciona saida do cliente à lista
+	       PrintStream ps = new PrintStream(cliente.getOutputStream());
+	       this.clientes.add(ps);
+	       
+	       // cria tratador de cliente numa nova thread
+	       TrataCliente tc = 
+	           new TrataCliente(cliente.getInputStream(), this);
+	       new Thread(tc).start();
+	     }
+	 
+	   }
+	 
+	   public void distribuiMensagem(String msg) {
+	     // envia msg para todo mundo
+	     for (PrintStream cliente : this.clientes) {
+	       cliente.println(msg);
+	     }
+	   }
+	 }
 
-	public static void main(String args[]) throws IOException {
-
-		ServerSocket servidor = new ServerSocket(12345);
-		System.out.println("Porta 12345 aberta!");
-
-		Socket cliente = servidor.accept();
-		System.out.println("Nova conexão com o cliente " + cliente.getInetAddress().getHostAddress());
-
-		Scanner inputCliente = new Scanner(cliente.getInputStream());
-		PrintStream saida = new PrintStream(cliente.getOutputStream());
-
-		while (inputCliente.hasNextLine()) {
-			String[] parts = inputCliente.nextLine().split(" ");
-			System.out.println("op " + parts[OPERACAO]);
-			int num1 = Integer.parseInt(parts[NUMERO1]);
-			int num2 = Integer.parseInt(parts[NUMERO2]);
-
-			analisarRequisicao(saida, parts, num1, num2);
-		}
-
-		inputCliente.close();
-		servidor.close();
-		cliente.close();
-	}
-
-	private static void analisarRequisicao(PrintStream saida, String[] parts, int num1, int num2) {
-		if (parts[OPERACAO].equals("add")) {
-			int soma = num1 + num2;
-			saida.println("0 " + soma);
-		} else if (parts[OPERACAO].equals("sub")) {
-			int sub = num1 - num2;
-			saida.println("0 " + sub);
-		} else if (parts[OPERACAO].equals("mul")) {
-			int mul = num1 * num2;
-			saida.println("0 " + mul);
-		} else if (parts[OPERACAO].equals("div") && num2 != 0) {
-			int div = num1/num2;
-			saida.println("0 " + div);
-		} else {
-			saida.println("1");
-		}
-	}
-}
+//public class Servidor {
+//	private static final int NUMERO2 = 2;
+//	private static final int NUMERO1 = 1;
+//	private static final int OPERACAO = 0;
+//
+//	public static void main(String args[]) throws IOException {
+//
+//		ServerSocket servidor = new ServerSocket(12345);
+//		System.out.println("Porta 12345 aberta!");
+//
+//		Socket cliente = servidor.accept();
+//		System.out.println("Nova conexão com o cliente " + cliente.getInetAddress().getHostAddress());
+//
+//		Scanner inputCliente = new Scanner(cliente.getInputStream());
+//		PrintStream saida = new PrintStream(cliente.getOutputStream());
+//
+//		while (inputCliente.hasNextLine()) {
+//			String[] parts = inputCliente.nextLine().split(" ");
+//			System.out.println("op " + parts[OPERACAO]);
+//			int num1 = Integer.parseInt(parts[NUMERO1]);
+//			int num2 = Integer.parseInt(parts[NUMERO2]);
+//
+//			analisarRequisicao(saida, parts, num1, num2);
+//		}
+//
+//		inputCliente.close();
+//		servidor.close();
+//		cliente.close();
+//	}
+//
+//	private static void analisarRequisicao(PrintStream saida, String[] parts, int num1, int num2) {
+//		if (parts[OPERACAO].equals("add")) {
+//			int soma = num1 + num2;
+//			saida.println("0 " + soma);
+//		} else if (parts[OPERACAO].equals("sub")) {
+//			int sub = num1 - num2;
+//			saida.println("0 " + sub);
+//		} else if (parts[OPERACAO].equals("mul")) {
+//			int mul = num1 * num2;
+//			saida.println("0 " + mul);
+//		} else if (parts[OPERACAO].equals("div") && num2 != 0) {
+//			int div = num1/num2;
+//			saida.println("0 " + div);
+//		} else {
+//			saida.println("1");
+//		}
+//	}
+//}
